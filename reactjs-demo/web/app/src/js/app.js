3cfcceb1bffec90a71f206cda302cbd82ded8fa6
fportela <fportela@tiempodevelopment.com>
2016-06-21 11:04:51 -0700

    removed wakanda client from app.js

--- reactjs-demo/web/app/src/js/app.js
+++ reactjs-demo/web/app/src/js/app.js
@@ -2,12 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoList from './components/TodoList';
 
-import { WakandaClient } from 'wakanda-client';
-
-let client = new WakandaClient();
-let ds;
-client.getCatalog(['Todos']).then(ds => {
-    ds = ds;
-    ReactDOM.render(<TodoList />, document.getElementById('app'));
-
-});
+ReactDOM.render(<TodoList />, document.getElementById('app'));
